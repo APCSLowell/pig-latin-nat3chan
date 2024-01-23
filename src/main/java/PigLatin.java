b4ad47368b03814d261e83cbbf09dd4767f00fc6
nat3chan <142937438+nat3chan@users.noreply.github.com>
2024-01-23 13:51:07 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -39,6 +39,8 @@ public int findFirstVowel(String word){
         //precondition: sWord is a valid String of length greater than 0
         //postcondition: returns the pig latin equivalent of sWord
         if(findFirstVowel(sWord) > -1){
+         if(findFirstVowel(sWord)==0){return sWord.substring(findFirstVowel(sWord),sWord.length())+sWord.substring(0,findFirstVowel(sWord))+"way";}
+         if(sWord.substring(0,2).equals(qu)){return sWord.substring(2,sWord.length())+"qu";}
         return sWord.substring(findFirstVowel(sWord),sWord.length())+sWord.substring(0,findFirstVowel(sWord))+"ay";}
       if(findFirstVowel(sWord) == -1) {
         return sWord + "ay";
